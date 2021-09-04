fc1dc497b7d5ef14576e064c1bc25a909f0efbd8
TheWicher1992 <sameer.nadeem24@gmail.com>
2021-09-04 18:38:23 +0500

    Home and Product screen connected to backend

--- client/src/screens/Product.js
+++ client/src/screens/Product.js
@@ -1,9 +1,19 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import products from "../products";
 import Rating from "../components/Rating";
+import axios from 'axios'
 const Product = ({ match }) => {
-  const product = products.find((p) => p._id === match.params.id);
+  const [product, setProduct] = useState({})
+
+  useEffect(() => {
+    const fetchProduct = async () => {
+      const { data } = await axios.get(`/api/products/${match.params.id}`)
+      setProduct(data)
+    }
+    fetchProduct()
+  }, [match])
+
+
   return (
     <>
       <Link to="/" className="btn btn-outline-dark">
